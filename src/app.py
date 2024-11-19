1c323f137058c8ce0346c021d39467ff8ffa8dcb
Nath1710 <nathalyhernandez815@gmail.com>
2024-11-18 20:38:27 -0500

    correct indentation

--- src/app.py
+++ src/app.py
@@ -8,8 +8,8 @@ todos = [
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-  json_text = jsonify(todos)
-  return json_text, 200
+    json_text = jsonify(todos)
+    return json_text, 200
 
 @app.route('/todos', methods=['POST'])
 def add_new_todo():
@@ -17,7 +17,7 @@ def add_new_todo():
     print("Incoming request with the following body:", request_body)
     todos.append(request_body)
     return jsonify(todos), 201
-  
+
 @app.route('/todos/<int:position>', methods=['DELETE'])
 def delete_todo(position):
     print("This is the position to delete:", position)
@@ -26,6 +26,6 @@ def delete_todo(position):
         return jsonify(todos)
     else:
         return jsonify({"error": "Position out of range"}), 400
-  
+
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
